4a0eacf332f202ffe72022f6378d6bef97ec2bdd
Helen Silva <helen_silva32@hotmail.com>
2024-03-02 19:11:12 -0300

    portfolio update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavBar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+    mobileNavBar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+    if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+    return navbar.classList.remove('active');
+});
+
+
 const form = document.querySelector("#form-contact");
 const buttonForm = document.querySelector("#button-form");
 
